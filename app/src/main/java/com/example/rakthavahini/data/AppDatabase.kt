9fd5e388d6dd4af8ed12c8353a90b14920a883a0
Tejashwini0505 <tejashwinis0505@gmail.com>
2026-05-15 18:59:41 +0530

    updated app database

--- app/src/main/java/com/example/rakthavahini/data/AppDatabase.kt
+++ app/src/main/java/com/example/rakthavahini/data/AppDatabase.kt
@@ -5,21 +5,45 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Donor::class, DonationRecord::class], version = 1)
+@Database(
+    entities = [
+        Donor::class,
+        DonationRecord::class
+    ],
+    version = 2
+)
+
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun donorDao(): DonorDao
 
     companion object {
-        @Volatile private var INSTANCE: AppDatabase? = null
 
-        fun getInstance(context: Context): AppDatabase {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(
+            context: Context
+        ): AppDatabase {
+
             return INSTANCE ?: synchronized(this) {
-                Room.databaseBuilder(
-                    context.applicationContext,
-                    AppDatabase::class.java,
-                    "rakta_vahini_db"
-                ).build().also { INSTANCE = it }
+
+                val instance =
+                    Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "rakta_vahini_db"
+                    )
+
+                        // IMPORTANT FIX
+
+                        .fallbackToDestructiveMigration()
+
+                        .build()
+
+                INSTANCE = instance
+
+                instance
             }
         }
     }
